34768696a9ff0c1ae02e084768620866df483041
Benedek Szabó <bndkszabo@gmail.com>
2024-03-14 09:31:52 +0100

    Made a time converter to hours, minutes and seconds, for more convinient time displaying.

--- progressbar.c
+++ progressbar.c
@@ -8,8 +8,35 @@ double time2 = 0;
 
 unsigned int tCounter = 1;
 
-double eta = 0;
-double *pEta = &eta;
+int eta = 0;
+int *pEta = &eta;
+
+
+struct Time {
+    int hours;
+    int minutes;
+    int seconds;
+};
+
+
+struct Time convertSeconds(int total_seconds) {
+    struct Time time;
+
+    time.hours = total_seconds / 3600;  // Calculate hours
+    time.minutes = (total_seconds % 3600) / 60;  // Calculate minutes
+    time.seconds = total_seconds % 60;  // Calculate remaining seconds
+
+    return time;  // Return the struct with converted time values
+}
+
+
+
+
+
+
+
+
+
 
 
 void remainingTime(double time, double *time1, double *time2, unsigned int *tCounter, int total, int progress) {
@@ -51,6 +78,8 @@ void progressbar(int progress, int total, int length, double time) {
     short unsigned int dotcounter = progress % 4; // Counts 0 -> 3 without additional variable added
     
 
+    struct Time realTime = convertSeconds(eta);
+
 
     if (length >= 30) {
             
@@ -66,7 +95,7 @@ void progressbar(int progress, int total, int length, double time) {
             }
         }
         remainingTime(time, &time1, &time2, &tCounter, total, progress);
-        printf("] %.0f%% Time remaining: %.3lfs", progressPercent, *pEta);
+        printf("] %.0f%% eta: %dh %dm %ds", progressPercent, realTime.hours, realTime.minutes, realTime.seconds);
         fflush(stdout);
         
         
