43b03b760491cd6b23944bb93490e810a6cf3bba
Benedek Szabó <bndkszabo@gmail.com>
2024-03-31 21:27:17 +0200

    It will display calc... in more cases, avoiding incorrect time display

--- progressbar.c
+++ progressbar.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+const unsigned short int needAveraging = 25; // Yeah, I'm bad at naming stuff
+
 double time1 = 0;
 double time2 = 0;
 unsigned int tCounter = 1;
@@ -11,6 +13,10 @@ unsigned int eta0 = 0;
 unsigned int eta1 = 0;
 unsigned int eta2 = 0;
 unsigned int eta3 = 0;
+unsigned int eta4 = 0;
+unsigned int eta5 = 0;
+unsigned int eta6 = 0;
+unsigned int eta7 = 0;
 short unsigned int counter;
 
 // somehow update the ints separately based on the counter
@@ -48,7 +54,15 @@ void remainingTime(double time, double* time1, double* time2, unsigned int* tCou
     printDebugPlusInt("*time2 = ", *time2);
 
     // Latest stuff... first 3 time incorrect
-    if (counter == 0) {
+    if (total <= needAveraging) {
+        if (*time1 && *time2 != 0) {
+            if (*tCounter == 1) {
+                *(&eta) = (total - progress) * ((*time1) - (*time2));
+            } else {
+                *(&eta) = (total - progress) * ((*time2) - (*time1));
+            }
+        }
+    } else if (counter == 0) {
         if (*time1 && *time2 != 0) {
             if (*tCounter == 1) {
                 *(&eta0) = (total - progress) * ((*time1) - (*time2));
@@ -84,17 +98,44 @@ void remainingTime(double time, double* time1, double* time2, unsigned int* tCou
             }
             *(&eta) = (*(&eta0) + *(&eta1) + *(&eta2) + *(&eta3)) / 4; // average calc
         }
+    } else if (counter == 4) {
+        if (*time1 && *time2 != 0) {
+            if (*tCounter == 1) {
+                *(&eta4) = (total - progress) * ((*time1) - (*time2));
+            } else {
+                *(&eta4) = (total - progress) * ((*time2) - (*time1));
+            }
+            *(&eta) = *(&eta0); // average calc
+        }
+    } else if (counter == 5) {
+        if (*time1 && *time2 != 0) {
+            if (*tCounter == 1) {
+                *(&eta5) = (total - progress) * ((*time1) - (*time2));
+            } else {
+                *(&eta5) = (total - progress) * ((*time2) - (*time1));
+            }
+            *(&eta) = (*(&eta0) + *(&eta1)) / 2; // average calc
+        }
+    } else if (counter == 6) {
+        if (*time1 && *time2 != 0) {
+            if (*tCounter == 1) {
+                *(&eta6) = (total - progress) * ((*time1) - (*time2));
+            } else {
+                *(&eta6) = (total - progress) * ((*time2) - (*time1));
+            }
+            *(&eta) = (*(&eta0) + *(&eta1) + *(&eta2)) / 3; // average calc
+        }
+    } else if (counter == 7) {
+        if (*time1 && *time2 != 0) {
+            if (*tCounter == 1) {
+                *(&eta7) = (total - progress) * ((*time1) - (*time2));
+            } else {
+                *(&eta7) = (total - progress) * ((*time2) - (*time1));
+            }
+            *(&eta) = (*(&eta0) + *(&eta1) + *(&eta2) + *(&eta3)) / 4; // average calc
+        }
     }
 
-
-
-    // if (*time1 && *time2 != 0) {
-    //     if (*tCounter == 1) {
-    //         *(&eta) = (total - progress) * ((*time1) - (*time2));
-    //     } else {
-    //         *(&eta) = (total - progress) * ((*time2) - (*time1));
-    //     }
-    // }
 
     (*tCounter)++;
 
@@ -127,7 +168,7 @@ void progressbar(int progress, int total, int length, double time)
             }
         }
         remainingTime(time, &time1, &time2, &tCounter, total, progress);
-        if (progress == 1 || progress == 2) { // This is the first run
+        if (((progress == 1 || progress == 2) && total <= needAveraging) || ((progress == 1 || progress == 2 || progress == 3 || progress == 4 || progress == 5) && total >= needAveraging)) { // This is the first run
             printf("] %.0f%% eta: calc...", progressPercent);
         } else {
             printf("] %.0f%% eta: %02d:%02d:%02d", progressPercent, realTime.hours, realTime.minutes, realTime.seconds);
