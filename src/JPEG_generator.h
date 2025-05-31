82ebd435ef10b80a0f3303b051efbc8cda887434
Benedek Szabó <bndkszabo@gmail.com>
2025-05-31 19:10:49 +0200

    Did I make the compiler happy?

--- src/JPEG_generator.h
+++ src/JPEG_generator.h
@@ -1,6 +1,7 @@
 #ifndef JPEG_GENERATOR
 #define JPEG_GENERATOR
+#include <stdbool.h>
 #include <stdint.h>
 
-void generateJPEG(char *filename, long width, long height, int quality, uint8_t r, uint8_t g, uint8_t b, bool random_multiplier);
+void generateJPEG(char* filename, long width, long height, int quality, uint8_t r, uint8_t g, uint8_t b, bool random_multiplier);
 #endif
